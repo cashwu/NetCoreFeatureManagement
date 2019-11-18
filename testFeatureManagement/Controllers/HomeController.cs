f927854699a38721dde54630958aa1ab9a3df0fe
cash <cash@cashwu.com>
2019-11-18 13:39:03 +0800

    update

--- testFeatureManagement/Controllers/HomeController.cs
+++ testFeatureManagement/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
 
 namespace testFeatureManagement.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IFeatureManager _featureManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFeatureManager featureManager)
         {
             _logger = logger;
+            _featureManager = featureManager;
         }
 
         public IActionResult Index()
         {
+            Console.WriteLine(_featureManager.IsEnabled("Beta"));
             return View();
+        }
+
+        [FeatureGate("Beta")]
+        public IActionResult Feature()
+        {
+            return Content("Feature");
         }
     }
 }
